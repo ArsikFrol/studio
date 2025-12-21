e701fcf84833410a1367c800e31e8c5b238a531e
ArseniyFrolov <arsik.frol.555@gmail.com>
2025-12-21 12:23:51 +0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import AboutUs from "@/components/AboutUs";
 import CasesAll from "@/components/CasesAll";
 import DiscussProject from "@/components/DiscussProject";
@@ -8,22 +6,16 @@ import Percentages from "@/components/Percentages";
 import Services from "@/components/Services";
 import Stages from "@/components/Stages";
 
-import p from '../../public/photo_2025-12-20_21-09-36.jpg'
-
 export default function Home() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Percentages />
       <Stages />
       <AboutUs />
       <CasesAll />
       <Services />
-      <DiscussProject /> */}
-      <div className=''>
-        Любимая Виктория
-        <Image className="block mx-auto py-[100px]" src={p} alt='' width={1000} height={1000} draggable='false' />
-      </div>
+      <DiscussProject />
     </>
   );
 }
